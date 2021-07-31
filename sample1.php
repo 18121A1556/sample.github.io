37576d1aed03a5f05ef366de52630546a82d09f1
Smaran-13 <65707260+Smaran-13@users.noreply.github.com>
2021-07-31 15:33:55 +0530

    Add files via upload

---
+++ sample1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="post">
+	<input type="Password" name="pwd" placeholder="Password">
+	<input type="submit" name="submit">
+	<?php
+	if(isset($_POST['submit']))
+				{
+					$fn=$_REQUEST['pwd'];
+					$ln=1234;
+					if($fn == $ln)
+					{
+						echo '<script>
+			       				window.location="sample.php";
+
+			            		</script>'; 
+					}
+					else
+					{
+						echo '<script>
+			       				alert("Incorrect password")
+
+			            		</script>'; 
+					}
+				}
+	?>
+</body>
+</html>
