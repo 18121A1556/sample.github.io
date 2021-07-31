b90cb7525079d3ee15bf1399940e0e02a5cf3407
Smaran-13 <65707260+Smaran-13@users.noreply.github.com>
2021-07-31 15:44:20 +0530

    Delete sample.php

--- sample.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body><iframe src="https://drive.google.com/file/d/1vL3QUmsp8MRbF7EEo89hlVa8vNkVOJ_U/preview" width="640" height="480" allow="autoplay"></iframe>
-</body>
-</html>
